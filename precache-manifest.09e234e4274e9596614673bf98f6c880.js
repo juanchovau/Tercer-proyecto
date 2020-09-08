6ec816f6629950a72e55b5a9cdeab5d4b3b295f2
Juancho <j.s.vargas.arevalo@gmail.com>
2020-09-08 17:28:31 -0500

    Updates

---
+++ precache-manifest.09e234e4274e9596614673bf98f6c880.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba8840964af302930f05ce3aff7c5b0",
+    "url": "/Tercer-proyecto/index.html"
+  },
+  {
+    "revision": "0f5ff801aad65f1f5708",
+    "url": "/Tercer-proyecto/static/css/main.9190a66b.chunk.css"
+  },
+  {
+    "revision": "a935a1de2214ea476ca8",
+    "url": "/Tercer-proyecto/static/js/2.22348478.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/Tercer-proyecto/static/js/2.22348478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5ff801aad65f1f5708",
+    "url": "/Tercer-proyecto/static/js/main.3a676b2b.chunk.js"
+  },
+  {
+    "revision": "6929d3d19a00a1680641",
+    "url": "/Tercer-proyecto/static/js/runtime-main.df843569.js"
+  },
+  {
+    "revision": "729eef121098dc98d0b181e43205598e",
+    "url": "/Tercer-proyecto/static/media/coin.729eef12.svg"
+  },
+  {
+    "revision": "b11a152020d4fee2affcf7a1482e211b",
+    "url": "/Tercer-proyecto/static/media/energy.b11a1520.svg"
+  },
+  {
+    "revision": "68402e7859e095bcef10e097b6b5f1a6",
+    "url": "/Tercer-proyecto/static/media/header.68402e78.png"
+  },
+  {
+    "revision": "d37b020e87945ad7f245e48df752ed03",
+    "url": "/Tercer-proyecto/static/media/perfil.d37b020e.jpg"
+  }
+]);
